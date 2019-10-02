c8ba0c14d50158c0ba5e199f308554c7e9d12c0c
Janaka Nanayakkara <janaka_nanayakkara@aol.com>
2019-10-02 22:56:37 +0530

    Working and finalized of 1.0

---
+++ FaceDetectionandRecognition1.0/FaceDetectionandRecognition1.0/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FaceDetectionandRecognition1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CameraBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Location = new System.Drawing.Point(28, 42);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(307, 286);
+            this.CameraBox.TabIndex = 2;
+            this.CameraBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(391, 55);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(224, 55);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Detection and Recognition";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(501, 268);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(114, 45);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save Face";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(430, 224);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(185, 20);
+            this.textName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CameraBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CameraBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
